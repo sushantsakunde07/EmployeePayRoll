f3fd6bc85624091a2161bc870e80406f31681ffd
suhantsakunde07 <sushantsakunde07@gmail.com>
2022-08-29 10:48:38 +0530

    Added: Exception Handling for name and salary

--- src/main/java/com/bridgelabz/employeepayroll/dto/EmployeePayrollDTO.java
+++ src/main/java/com/bridgelabz/employeepayroll/dto/EmployeePayrollDTO.java
@@ -1,12 +1,16 @@
 package com.bridgelabz.employeepayroll.dto;
 
 import lombok.Data;
+
+import javax.validation.constraints.Min;
 import javax.validation.constraints.Pattern;
 @Data
 public class EmployeePayrollDTO {
     @Pattern(regexp = "^[A-Z]{1}[a-zA-Z\\s]{2,}$", message = "Employee name is Invalid")
     public String name;
-    public long salary;
+    @Min(value = 500, message = "Min Wage should be more than 500")
+    public  long salary;
+
 
     public EmployeePayrollDTO(String name, long salary) {
         this.name = name;
