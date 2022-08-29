07fd518dc58cd0e67e8efa1db542ddd0d320c98d
suhantsakunde07 <sushantsakunde07@gmail.com>
2022-08-29 09:18:50 +0530

    Added: Validation to name field

--- src/main/java/com/bridgelabz/employeepayroll/controller/EmployeePayRollAppController.java
+++ src/main/java/com/bridgelabz/employeepayroll/controller/EmployeePayRollAppController.java
@@ -9,48 +9,48 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import javax.validation.Valid;
 import java.util.List;
 
 @RestController
-@RequestMapping("/employeepayrollservice")
-public class EmployeePayrollAppController {
-
+@RequestMapping ("/employeepayrollservice")
+public class EmployeePayRollAppController {
     @Autowired
     private EmployeePayrollService employeePayrollService;
-
-    @RequestMapping(value = {"", "/", "/get"})
-    public ResponseEntity<ResponseDTO> getEmployeePayrollData() {
+    @RequestMapping(value = {"","/","/get"})
+    public ResponseEntity<ResponseDTO> getEmployeePayrollData(){
         List<Employee> employeeList = null;
         employeeList = employeePayrollService.getEmployeeDetails();
-        ResponseDTO responseDTO = new ResponseDTO("Get Call Sucess", employeeList);
-        return new ResponseEntity<ResponseDTO>(responseDTO, HttpStatus.OK);
+        ResponseDTO responseDto = new ResponseDTO("Get Call Sucess", employeeList);
+        return new ResponseEntity<ResponseDTO>(responseDto, HttpStatus.OK);
     }
 
     @GetMapping("/get/{empId}")
-    public ResponseEntity<ResponseDTO> getEmployeePayrollData(@PathVariable("empId") int empId) {
+    public ResponseEntity<ResponseDTO> getEmployeePayrollData(@PathVariable("empId")int empId){
         Employee employee = employeePayrollService.getEmployeeDetailsById(empId);
         ResponseDTO responseDTO = new ResponseDTO("Get Call For Id Sucessful", employee);
         return new ResponseEntity<ResponseDTO>(responseDTO, HttpStatus.OK);
     }
 
     @PostMapping("/create")
-    public ResponseEntity<ResponseDTO> addEmployeePayrollData(@RequestBody EmployeePayrollDTO empPayrollDTO) {
+    public ResponseEntity<ResponseDTO> addEmployeePayrollData(@Valid @RequestBody EmployeePayrollDTO empPayrollDTO){
         Employee employee = employeePayrollService.CreateEmployeeDetails(empPayrollDTO);
         ResponseDTO responseDTO = new ResponseDTO("Created Employee Payroll Data Successfully:", employee);
         return new ResponseEntity<ResponseDTO>(responseDTO, HttpStatus.OK);
     }
 
     @PutMapping("/update/{empId}")
-    public ResponseEntity<ResponseDTO> updateEmployeePayrollData(@PathVariable("empId") int empId, @RequestBody EmployeePayrollDTO empPayrollDTO) {
+    public ResponseEntity<ResponseDTO> updateEmployeePayrollData(@Valid @PathVariable("empId") int empId, @RequestBody EmployeePayrollDTO empPayrollDTO){
         Employee employee = employeePayrollService.updateEmployeeDetails(empId, empPayrollDTO);
         ResponseDTO responseDTO = new ResponseDTO("Updated Employee Payroll Data Successfully:", employee);
         return new ResponseEntity<ResponseDTO>(responseDTO, HttpStatus.OK);
     }
 
     @DeleteMapping("/delete/{empId}")
-    public ResponseEntity<ResponseDTO> deleteEmployeePayrollData(@PathVariable("empId") int empId) {
+    public ResponseEntity<ResponseDTO> deleteEmployeePayrollData(@PathVariable("empId")int empId){
         employeePayrollService.deleteEmployeeDetails(empId);
-        ResponseDTO responseDTO = new ResponseDTO("Deleted Successfully:", "Delete Id:" + empId);
-        return new ResponseEntity<ResponseDTO>(responseDTO, HttpStatus.OK);
+        ResponseDTO responseDTO = new ResponseDTO("Deleted Successfully:","Delete Id:"+empId);
+        return new ResponseEntity<ResponseDTO>(responseDTO,HttpStatus.OK);
+
     }
 }
--- src/main/java/com/bridgelabz/employeepayroll/dto/EmployeePayrollDTO.java
+++ src/main/java/com/bridgelabz/employeepayroll/dto/EmployeePayrollDTO.java
@@ -1,16 +1,18 @@
 package com.bridgelabz.employeepayroll.dto;
-import lombok.Data;
 
+import lombok.Data;
+import javax.validation.constraints.Pattern;
 @Data
 public class EmployeePayrollDTO {
-
+    @Pattern(regexp = "^[A-Z]{1}[a-zA-Z\\s]{2,}$", message = "Employee name is Invalid")
     public String name;
-    public  long salary;
+    public long salary;
 
     public EmployeePayrollDTO(String name, long salary) {
         this.name = name;
         this.salary = salary;
     }
+
     @Override
     public String toString() {
         return "EmployeePayrollDTO{" +
