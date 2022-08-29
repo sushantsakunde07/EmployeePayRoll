a3bb450c99693c8164e2067bd1f162ff2eb07350
suhantsakunde07 <sushantsakunde07@gmail.com>
2022-08-29 10:58:27 +0530

    Refactored: Custom exception for employee not found

---
+++ src/main/java/com/bridgelabz/employeepayroll/EmployeePayRollApplication.java
@@ -0,0 +1,14 @@
+package com.bridgelabz.employeepayroll;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmployeePayRollApplication {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+        SpringApplication.run(EmployeePayRollApplication.class, args);
+    }
+
+}
