104ce21f211167533e35ad20637a854ecdc46824
suhantsakunde07 <sushantsakunde07@gmail.com>
2022-08-29 12:21:17 +0530

    Added: Validation property on PayrollDTO

--- src/main/java/com/bridgelabz/employeepayroll/dto/EmployeePayrollDTO.java
+++ src/main/java/com/bridgelabz/employeepayroll/dto/EmployeePayrollDTO.java
@@ -1,10 +1,11 @@
 package com.bridgelabz.employeepayroll.dto;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
 import lombok.Data;
+import lombok.NonNull;
 import lombok.ToString;
 
-import javax.validation.constraints.Min;
-import javax.validation.constraints.Pattern;
+import javax.validation.constraints.*;
 import java.time.LocalDate;
 import java.util.List;
 
@@ -14,10 +15,17 @@ public class EmployeePayrollDTO {
     public String name;
     @Min(value = 500, message = "Min Wage should be more than 500")
     public long salary;
+    @Pattern(regexp = "male|female", message = "Gender needs to be male or female")
     public String gender;
+    @JsonFormat(pattern = "dd MMM yyyy")
+    @NotNull(message = "Start Date should not be Empty")
+    @PastOrPresent(message = "Start Date should be past or todays date")
     public LocalDate startDate;
+    @NotBlank(message = "Note cannot be Empty")
     public String note;
+    @NotBlank(message = "Profile Pic cannot be Empty")
     public String profilePic;
+    @NotNull(message = "Department should not be Empty")
     public List<String> department;
 
 }
